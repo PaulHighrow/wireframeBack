91b63555f0fce5be6027dee1b4077532fcc946ca
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2023-07-31 10:04:52 +0300

    updated validation rules

--- schema/leadSchema.js
+++ schema/leadSchema.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const leadSchema = Joi.object({
   name: Joi.string().min(3).max(50).required(),
-  phone: Joi.string().min(3).max(13),
+  phone: Joi.string().min(3).max(20).required(),
 });
 
 const validateLead = ({ body }, res, next) => {
